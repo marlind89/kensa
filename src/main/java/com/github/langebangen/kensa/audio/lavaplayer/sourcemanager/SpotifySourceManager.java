257f1ff8f321b20496909e764b5418871ce285a5
Martin Lindgren <martin.l@totalproduce.se>
2021-07-29 21:21:20 +0200

    Bump lavaplayer/d4j versions

--- src/main/java/com/github/langebangen/kensa/audio/lavaplayer/sourcemanager/SpotifySourceManager.java
+++ src/main/java/com/github/langebangen/kensa/audio/lavaplayer/sourcemanager/SpotifySourceManager.java
@@ -1,5 +1,17 @@
 package com.github.langebangen.kensa.audio.lavaplayer.sourcemanager;
 
+import com.github.langebangen.kensa.audio.lavaplayer.YoutubeBestMatchAudioTrack;
+import com.google.inject.Inject;
+import com.neovisionaries.i18n.CountryCode;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider;
+import com.sedmelluq.discord.lavaplayer.track.*;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.*;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
@@ -7,26 +19,6 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.github.langebangen.kensa.audio.lavaplayer.YoutubeBestMatchAudioTrack;
-import com.google.inject.Inject;
-import com.neovisionaries.i18n.CountryCode;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider;
-import com.sedmelluq.discord.lavaplayer.track.AudioItem;
-import com.sedmelluq.discord.lavaplayer.track.AudioReference;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
-import com.sedmelluq.discord.lavaplayer.track.BasicAudioPlaylist;
-import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
-import com.wrapper.spotify.model_objects.specification.Paging;
-import com.wrapper.spotify.model_objects.specification.Playlist;
-import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
-import com.wrapper.spotify.model_objects.specification.Track;
 
 public class SpotifySourceManager
 	implements AudioSourceManager
@@ -53,8 +45,7 @@ public class SpotifySourceManager
 	}
 
 	@Override
-	public AudioItem loadItem(DefaultAudioPlayerManager manager, AudioReference reference)
-	{
+	public AudioItem loadItem(AudioPlayerManager manager, AudioReference reference) {
 		String identifier = reference.identifier;
 
 		if (!identifier.matches("spotify:playlist:.+"))
@@ -67,36 +58,36 @@ public class SpotifySourceManager
 		try
 		{
 			Playlist playlist = spotifyApi.getPlaylist(playlistId)
-				.market(CountryCode.SE)
-				.build()
-				.execute();
+					.market(CountryCode.SE)
+					.build()
+					.execute();
 			List<AudioTrack> audioTracks = new LinkedList<>();
 			int total = Integer.MAX_VALUE;
 			int offset = 0;
 			while (total > offset)
 			{
 				Paging<PlaylistTrack> playlistTracks = spotifyApi.getPlaylistsTracks(playlistId)
-					.market(CountryCode.SE)
-					.limit(100)
-					.offset(offset)
-					.build()
-					.execute();
+						.market(CountryCode.SE)
+						.limit(100)
+						.offset(offset)
+						.build()
+						.execute();
 
 				total = playlistTracks.getTotal();
 
 				audioTracks.addAll(Arrays.stream(playlistTracks.getItems())
-					.map(playlistTrack ->
-					{
-						Track track = playlistTrack.getTrack();
+						.map(playlistTrack ->
+						{
+							Track track = playlistTrack.getTrack();
 
-						ArtistSimplified[] artists = track.getArtists();
-						String firstArtistName = artists.length == 0 ? "" : artists[0].getName();
+							ArtistSimplified[] artists = track.getArtists();
+							String firstArtistName = artists.length == 0 ? "" : artists[0].getName();
 
-						return new YoutubeBestMatchAudioTrack(new AudioTrackInfo(firstArtistName + " - " + track.getName(), firstArtistName,
-							track.getDurationMs(), "", false, ""),
-							ytAudioSourceManager, ytSearchProvider, ytAudioSourceManager);
-					})
-					.collect(Collectors.toList()));
+							return new YoutubeBestMatchAudioTrack(new AudioTrackInfo(firstArtistName + " - " + track.getName(), firstArtistName,
+									track.getDurationMs(), "", false, ""),
+									ytAudioSourceManager, ytSearchProvider, ytAudioSourceManager);
+						})
+						.collect(Collectors.toList()));
 
 				offset += playlistTracks.getLimit();
 			}
